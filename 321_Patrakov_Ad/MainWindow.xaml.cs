85f53534edd1ff2eb0db982dc9026adcf1fb3685
Данила Патраков <ddp2004@yandexru>
2025-01-22 08:55:22 +0300

    Добавьте файлы проекта.

---
+++ 321_Patrakov_Ad/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _321_Patrakov_Ad.Pages;
+
+namespace _321_Patrakov_Ad
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+                MainFrame.GoBack();
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Вы действительно хотите выйти?", "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+            base.OnClosing(e);
+        }
+
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            if (!(e.Content is Page page)) return;
+            this.Title = $"ProjectByPatrakov - {page.Title}";
+
+            if (page is Pages.AdsPage)
+                BackButton.Visibility = Visibility.Hidden;
+            else
+                BackButton.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ 321_Patrakov_Ad/Pages/AdsPage.xaml.cs
@@ -0,0 +1,113 @@
+﻿using _321_Patrakov_Ad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace _321_Patrakov_Ad.Pages
+{
+    public partial class AdsPage : Page
+    {
+        public AdsPage()
+        {
+            InitializeComponent();
+            LoadCategories();
+            LoadCities();
+            var currentAds = Entities.GetContext().Ads.ToList();
+            ListAds.ItemsSource = currentAds;
+            UpdateAds();
+        }
+
+        private void LoadCategories()
+        {
+            var categories = Entities.GetContext().Categories.ToList();
+            foreach (var category in categories)
+            {
+                CategoryBox.Items.Add(new ComboBoxItem { Content = category.category_name });
+            }
+        }
+
+        private void LoadCities()
+        {
+            var cities = Entities.GetContext().Cities.ToList();
+            foreach (var city in cities)
+            {
+                CityBox.Items.Add(new ComboBoxItem { Content = city.city_name });
+            }
+        }
+
+        private void UpdateAds()
+        {
+            var currentAds = Entities.GetContext().Ads.ToList();
+
+            if (!string.IsNullOrEmpty(SearchBox.Text))
+            {
+                currentAds = currentAds.Where(x => x.title.ToLower().Contains(SearchBox.Text.ToLower()) ||
+                                                    x.description.ToLower().Contains(SearchBox.Text.ToLower())).ToList();
+            }
+
+            if (CategoryBox.SelectedIndex > 0)
+            {
+                var selectedCategory = (CategoryBox.SelectedItem as ComboBoxItem).Content.ToString();
+                currentAds = currentAds.Where(x => x.Categories.category_name == selectedCategory).ToList();
+            }
+
+            if (CityBox.SelectedIndex > 0)
+            {
+                var selectedCity = (CityBox.SelectedItem as ComboBoxItem).Content.ToString();
+                currentAds = currentAds.Where(x => x.Cities.city_name == selectedCity).ToList();
+            }
+
+            if (TypeBox.SelectedIndex > 0)
+            {
+                var selectedType = (TypeBox.SelectedItem as ComboBoxItem).Content.ToString();
+                currentAds = currentAds.Where(x => x.Types.type_name == selectedType).ToList();
+            }
+
+            if (StatusBox.SelectedIndex > 0)
+            {
+                var selectedStatus = (StatusBox.SelectedItem as ComboBoxItem).Content.ToString();
+                currentAds = currentAds.Where(x => x.Statuses.status_name == selectedStatus).ToList();
+            }
+
+            ListAds.ItemsSource = currentAds;
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateAds();
+        }
+
+        private void CategoryBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAds();
+        }
+
+        private void CityBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAds();
+        }
+
+        private void TypeBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAds();
+        }
+
+        private void StatusBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAds();
+        }
+
+        private void ClearFilterButton_Click(object sender, RoutedEventArgs e)
+        {
+            SearchBox.Text = string.Empty;
+            CategoryBox.SelectedIndex = 0;
+            CityBox.SelectedIndex = 0;
+            TypeBox.SelectedIndex = 0;
+            StatusBox.SelectedIndex = 0;
+
+            UpdateAds();
+        }
+    }
+}
